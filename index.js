df28f2d6a2b4d036dc3980dbc5b9ba6587d6df3f
yashsagar <sagar.sagar75@gmail.com>
2024-01-05 00:37:36 +0530

    React feature added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 // createElement.textContent = "I am going to rock the word";
 // const selectElement = document.getElementById("root");
 // selectElement.appendChild(createElement);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const createElement = React.createElement(
   "h1",
   {},
-  "I am from React app updated"
+  "I am from React app updated!!! :)"
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(createElement);
